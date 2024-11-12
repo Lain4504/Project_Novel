0ef1b08e74e226ebe0aab71c8288eebbcfc851dc
Lain4504 <tienhuynhn2004@gmail.com>
2024-11-12 22:40:46 +0700

    Setup Project

---
+++ identity_service/src/main/java/com/backend/novel/identityservice/entity/User.java
@@ -0,0 +1,26 @@
+package com.backend.novel.identityservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+    String username;
+    String email;
+    String password;
+    LocalDate dateCreated;
+    @ManyToMany
+    Set<Role> roles;
+}
