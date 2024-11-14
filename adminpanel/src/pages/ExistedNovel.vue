5561ea6f68a07bbbc7d61fd4a5bd0aa1c1e0808a
Lain4504 <tienhuynhn2004@gmail.com>
2024-11-14 19:38:51 +0700

    FE - Create base layout and SideBar
    Create BE - UserProfileService

--- adminpanel/src/pages/ExistedNovel.vue
+++ adminpanel/src/pages/ExistedNovel.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 </script>
-
 <template>
-  
+    <div>
+        Existed Novel
+    </div>
 </template>
 
--- adminpanel/src/router/index.ts
+++ adminpanel/src/router/index.ts
@@ -1,11 +1,38 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Dashboard from '../pages/Dashboard.vue';
+import UserManagement from '../pages/UserManagement.vue';
+import Analysis from '../pages/Analysis.vue';
+import AddNovel from '../pages/AddNovel.vue';
+import ExistedNovel from '../pages/ExistedNovel.vue';
 const routes: Array<RouteRecordRaw> = [
-    {
-      name: 'dashboard',
-      path: '/',
-      component: Dashboard,
-    }
+  {
+    name: 'dashboard',
+    path: '/',
+    component: Dashboard,
+    children: [
+      {
+        name: 'analysis',
+        path: '/analysis',
+        component: Analysis,
+      },
+      {
+        name: 'usermanagement',
+        path: '/user-management',
+        component: UserManagement,
+      },
+      {
+        name: 'newnovel',
+        path: '/new-novel',
+        component: AddNovel,
+      },
+      {
+        name: 'existednovels',
+        path: '/existing-novels',
+        component: ExistedNovel,
+      },
+
+    ]
+  },
 ];
 
 // Tạo router
