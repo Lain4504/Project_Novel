11064464f1c9c8660a9505169449a86f5edad46b
Lain4504 <tienhuynhn2004@gmail.com>
2024-12-07 22:59:28 +0700

    Create FileService

--- common/src/main/java/com/backend/exception/ErrorCode.java
+++ common/src/main/java/com/backend/exception/ErrorCode.java
@@ -18,6 +18,10 @@ public enum ErrorCode {
     INCORRECT_CREDENTIALS("1009", "Incorrect email or password", HttpStatus.BAD_REQUEST),
     INACTIVE_USER("1010", "Account isn't active", HttpStatus.BAD_REQUEST),
     INVALID_REFRESH_TOKEN("1011", "Refresh token isn't invalid", HttpStatus.BAD_REQUEST),
+    FILE_NAME_IS_EMPTY("1012", "File name must not be null", HttpStatus.BAD_REQUEST),
+    FILE_EXTENSION_INVALID("1013", "File extension is invalid", HttpStatus.BAD_REQUEST),
+    FILE_SIZE_INVALID("1014", "File size is invalid", HttpStatus.BAD_REQUEST),
+    IMAGE_NOT_FOUND("1015", "Image not found", HttpStatus.NOT_FOUND),
 
     ;
 
--- file-service/src/main/java/com/backend/FileServiceApplication.java
+++ file-service/src/main/java/com/backend/FileServiceApplication.java
@@ -1,7 +1,13 @@
 package com.backend;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(scanBasePackages = "com.backend")
+@EnableFeignClients
 public class FileServiceApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(FileServiceApplication.class, args);
     }
 }
