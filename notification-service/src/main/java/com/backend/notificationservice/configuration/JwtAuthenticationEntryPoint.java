f379e12e9d549fa56950668bf59199eea9589f1a
Lain4504 <tienhuynhn2004@gmail.com>
2024-12-08 10:32:43 +0700

    NotificationService Setup

--- notification-service/src/main/java/com/backend/notificationservice/configuration/JwtAuthenticationEntryPoint.java
+++ notification-service/src/main/java/com/backend/notificationservice/configuration/JwtAuthenticationEntryPoint.java
@@ -2,20 +2,35 @@ package com.backend.notificationservice.configuration;
 
 import java.io.IOException;
 
+import com.backend.dto.response.ApiResponse;
+import com.backend.exception.ErrorCode;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.stereotype.Component;
 
-@Component
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
-
     @Override
     public void commence(
             HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
             throws IOException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+        ErrorCode errorCode = ErrorCode.UNAUTHENTICATED;
+
+        response.setStatus(errorCode.getStatusCode().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
+        response.flushBuffer();
     }
 }
