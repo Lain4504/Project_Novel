2a1a8f3d7e846e5f92e2769183163315e83e51ac
Lain4504 <tienhuynhn2004@gmail.com>
2024-12-13 16:54:18 +0700

    Upload Avatar for UserProfile

--- profile-service/src/main/java/com/backend/profileservice/ProfileServiceApplication.java
+++ profile-service/src/main/java/com/backend/profileservice/ProfileServiceApplication.java
@@ -2,7 +2,9 @@ package com.backend.profileservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication(scanBasePackages = "com.backend")
 public class ProfileServiceApplication {
     public static void main(String[] args) {
